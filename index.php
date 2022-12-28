72495bf27217426be36cb879d3be312474a22573
rangapin <rangapin@gmx.com>
2022-12-27 21:04:01 -0600

    initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+$books = [
+    [
+        'name' => 'Do Androids Dream of Electric Sheep',
+        'author' => 'Philip K. Dick',
+        'releaseYear' => 1968,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'Project Hail Mary',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2021,
+        'purchaseUrl' => 'http://example.com'
+    ],
+    [
+        'name' => 'The Martian',
+        'author' => 'Andy Weir',
+        'releaseYear' => 2011,
+        'purchaseUrl' => 'http://example.com'
+    ],
+];
+
+$filteredBooks = array_filter($books, function ($book) {
+    return $book['releaseYear'] >= 1950 && $book['releaseYear'] <= 2020;
+});
+
+require "index.view.php";
+
+<?php foreach ($books as $book) : ?>
+    <li>
+        <a href="<?= $book['purchaseUrl'] ?>">
+        <?= $book['name']; ?>
+    </a>
+    </li>
+<?php endforeach; ?>
+</body>
+</html>
